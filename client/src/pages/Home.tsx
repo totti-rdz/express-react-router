b863ec5fca9e759e7d0df3fd2ddd9519b0393b81
Thorsten Herfurtner <thorsten.herfurtner@empion.io>
2023-05-19 01:52:33 +0200

    add linking to the pages

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,13 @@
 import "../App.css";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <>
       <div>Hello world</div>
+      <div>
+        <Link to={`/other`}>Other Page</Link>
+      </div>
     </>
   );
 }
--- client/src/pages/Other.tsx
+++ client/src/pages/Other.tsx
@@ -1,9 +1,13 @@
 import "../App.css";
+import { Link } from "react-router-dom";
 
 function Other() {
   return (
     <>
       <div>Hello again</div>
+      <div>
+        <Link to={`/`}>Home</Link>
+      </div>
     </>
   );
 }
