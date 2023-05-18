9fd010f60033f5fe0614582f25fca65478b7cc41
Thorsten Herfurtner <thorsten.herfurtner@empion.io>
2023-05-19 01:12:44 +0200

    create Home + Other pages

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import "../App.css";
+
+function Home() {
+  return (
+    <>
+      <div>Hello world</div>
+    </>
+  );
+}
+
+export default Home;
---
+++ client/src/pages/Other.tsx
@@ -0,0 +1,11 @@
+import "../App.css";
+
+function Other() {
+  return (
+    <>
+      <div>Hello again</div>
+    </>
+  );
+}
+
+export default Other;
