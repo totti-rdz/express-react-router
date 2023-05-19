9987bb06d7bebd2ee9931294983473e2a426170b
Thorsten Herfurtner <thorsten.herfurtner@empion.io>
2023-05-19 02:35:50 +0200

    redirect every request to express to client

--- server.ts
+++ server.ts
@@ -6,6 +6,10 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, "client", "dist")));
 
+app.get("*", (_, res: Response) => {
+  res.sendFile(path.join(__dirname + "/client/dist/index.html"));
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
