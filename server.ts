7addab33a0c773a7e32b9216f3dfff285cdd5303
Thorsten Herfurtner <thorsten.herfurtner@empion.io>
2023-05-19 02:35:03 +0200

    serve client via express

--- server.ts
+++ server.ts
@@ -1,12 +1,10 @@
-import express from "express";
-import { Response } from "express";
+import express, { Response } from "express";
+import path from "path";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (_, res: Response) => {
-  res.send("Hello World!");
-});
+app.use(express.static(path.join(__dirname, "client", "dist")));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
